c5717a99408c9a7f6b320d334c7faff20704576a
Sebaslsst <leonardo.sanchez@unl.edu.ec>
2025-01-27 07:09:51 -0500

    Add files via upload

---
+++ Farmacia_POO/Farmacia_APP/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+
+# Home page
+def home(request):
+    return render(request, "home.html")
+def farmaceutico(request):
+    return render(request, "farmaceutico.html")
+def cliente(request):
+    return render(request, "cliente.html")
+def inventario(request):
+    return render(request, "inventario.html")
+def pedido(request):
+    return render(request, "pedido.html")
+def reporte(request):
+    return render(request, "reporte.html")
+def sucursal(request):
+    return render(request, "sucursal.html")
+def compra(request):
+    return render(request, "compra.html")
